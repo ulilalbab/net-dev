30d08f7c036be6f34fa946282f0dbd96be0d24db
ulil <ulil@protonmail.com>
2021-07-03 17:44:25 +0700

    update resources

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ By Marius Bancila, Raffaele Rialdi, Ankit Sharma
 April 2020
 
 https://subscription.packtpub.com/book/Programming/9781789805864
+
+
+
+Learning C# Fundamental Syntx 
+https://app.pluralsight.com/course-player?clipId=0fc3d387-1e0d-4b88-a382-22efd53e7e6b
---
+++ write-line.cs
@@ -0,0 +1,14 @@
+using System;
+namespace chapter_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("i will print on a new line.");
+            
+        }
+    }
+
+}
