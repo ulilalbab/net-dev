b77d333e2f83a8aec71250b2ab123c2112f2f971
ulil <ulil@protonmail.com>
2021-07-03 18:57:52 +0700

    part 1

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,20 @@
+﻿using System; //Day 1
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args) //Main is method, entry point of applications
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello, {args[0]} !"); //Put Input after first string
+            }
+            else
+            {
+                Console.WriteLine("hello!");
+
+            }
+        }
+    }
+}
