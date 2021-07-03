4b3bf094c1861365d20f42c5f7de7c5d6a40739e
ulil <ulil@protonmail.com>
2021-07-03 19:38:07 +0700

    little confuse :)

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -1,19 +1,30 @@
 ﻿using System; //Day 1
+using System.Collections.Generic;
 
 namespace GradeBook
 {
     class Program
     {
-        static void Main(string[] args) //Main is method, entry point of applications
+        static void Main(string[] args)                                         //Main is method, entry point of applications
         {
+                        var grades = new List<double>() {12.7,10.3,6.11,4.1};
+            grades.Add(56.1);
+           
+            
+            var result = 0.0;
+            foreach(var number in grades) 
+            {
+                    result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}");
             if (args.Length > 0)
             {
-                Console.WriteLine($"Hello, {args[0]} !"); //Put Input after first string
+                Console.WriteLine($"Hello, {args[0]} !");                           //Put Input after first string
             }
             else
             {
-                Console.WriteLine("hello!");
-
+                Console.WriteLine("Hello!");
             }
         }
     }
