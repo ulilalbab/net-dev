7e42c28e3ec8c3a78a1bfc31a9c0f3bfdb151962
ulil <ulil@protonmail.com>
2021-07-11 17:40:09 +0700

    chapter 4

---
+++ gradebook/src/GradeBook/Book.cs
@@ -0,0 +1,13 @@
+namespace GradeBook
+{
+    class Book
+    {
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+           List<double> grades;   
+    }
+}
+
+// Workingf With Classes and Objects (adding a Constructor)
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -1,16 +1,21 @@
-﻿using System; //Day 1
+﻿using System; //Second Meeting
 using System.Collections.Generic;
 
 namespace GradeBook
 {
+
     class Program
     {
         static void Main(string[] args)                                         //Main is method, entry point of applications
-        {
-                        var grades = new List<double>() {12.7,10.3,6.11,4.1};
-            grades.Add(56.1);
-           
+        {            
+            var book = new Book();
+            book.AddGrade(89.1);
             
+            var grades = new List<double>() {12.7,10.3,6.11,4.1};
+            grades.Add(56.1);
+            
+
+
             var result = 0.0;
             foreach(var number in grades) 
             {
@@ -18,14 +23,7 @@ namespace GradeBook
             }
             result /= grades.Count;
             Console.WriteLine($"The average grade is {result:N1}");
-            if (args.Length > 0)
-            {
-                Console.WriteLine($"Hello, {args[0]} !");                           //Put Input after first string
-            }
-            else
-            {
-                Console.WriteLine("Hello!");
-            }
+
         }
     }
 }
